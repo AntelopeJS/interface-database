b5aa441fe2de456fd157f7990e6da1209a5e64fe
Antony Rizzitelli <rizzitelli.antony@pm.me>
2026-09-14 22:57:45 +0000

    test: use published mongodb 1.3.0 for interface conformance

--- src/antelope.test.ts
+++ src/antelope.test.ts
@@ -11,7 +11,7 @@ export default defineConfig({
       source: {
         type: "package",
         package: "@antelopejs/mongodb",
-        version: "1.2.7",
+        version: "1.3.0",
       },
     },
   },
